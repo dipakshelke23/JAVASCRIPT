3f39906483ed15253459aa7bb1032ea0ac22b673
dipakshelke <dipakshelke2323@gmail.com>
2023-10-21 15:17:12 +0530

    VARIABLES

--- variables/variable.js
+++ variables/variable.js
@@ -1,4 +1,18 @@
-// let a=5;
-// var b=4;
-// let b=3;
-console.log('hello world')
+// A]
+// var = for var we can re-initialise and re-assign value to variable 
+var studentname = "myName";
+studentname="ram"
+var studentname="ram"
+
+// B] for let we can reassign value but we cant re-initialise variable
+let std=10;
+// let std=10;
+std=20
+
+
+// B] for const we canot reassign value and also  we cant re-initialise variable
+const id= 23;
+
+// console.log('studentname,std,id'); // print it as a string
+// console.log(typeof[studentname,std,id]);// object
+// console.log(studentname,std,id);
