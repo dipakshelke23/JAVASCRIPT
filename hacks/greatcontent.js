76d03b20975e523ca9973e436f64df5cefc80893
dipakshelke <dipakshelke2323@gmail.com>
2023-10-24 19:52:41 +0530

    LOOK AT THIS ONCE IN WEEK

---
+++ hacks/greatcontent.js
@@ -0,0 +1,55 @@
+// ****************************************** WHAT TO  DO WHEN I AM BORED ************************************************
+
+// Write a story and shape the characters however you love them to be, you can always publish it for free on the internet.
+// Read about some great people and understand their struggles and decisions, and learn.
+// Open that old photo album catching dust in the closet, some memories will bring tears of happiness in your eyes.
+// Open Quora and read about extra ordinary experiences of ordinary people, or read articles.
+// Read a newspaper, underline the words you don't understand, find their meaning and use it in a sentence.
+// Sit with your family, talk to them, listen them, share whatever you skipped in the never ending race of career and life.
+// Peacefully lie down on your comfortable bed and listen to romantic songs, loosen your body and relax.
+// Pick up certain issues, topics, news, which fascinate you or interest you and do a research on them, read about them and understand them well.
+// Clean your room, your closet, all drawers, arrange everything neatly, change locations, identify unneeded stuff and donate it.
+// Find sources of laughter, comedy videos, cartoons, jokes, laugher shows, more you laugh, more your system freshens up.
+// Make plans, decide your future, make travel plans, decide to meet old friends, imagine a world where you want to live so that you can create it.
+// Most important, do an introspection of your present life, your lifestyle, your behaviour, identify flaws and make a pledge to improve.
+// Keep aside your ego and talk to that old friend whom you miss everyday but never call.
+// Offer a head massage or a foot massage to your parents, or spouse, or grandparents, any lived one and make them super happy.
+// Free times are an opportunity to reset the existing life and routine, and freshen up.
+
+// **************************************How to improve intiligance*********************************************************
+
+// your subconscious(or i call it unconscious) is far more strategic and powerfull than you ever imagine. problem is we don't use it. 
+// It acts according to your beliefs. so believe in it. if you are not getting any answer to a question, 
+// leave it to your unconscious(there is no mantra. just believe and leave it). be conscious. and it gives you the answer. 
+// it can happen in many ways like in your dream, or like a flash of thought. or through your spoken words unwillingly. 
+// or it may create or give you a physical situation which leads you to the answer.
+
+// Without knowledge, intelligence is hollow. Learn as much as you can.
+// Never mug up. Ask, search and read again and again till you understand a concept.
+// Observe everything and everyone you see in routine life.
+// Don't aim at scoring or answering, think with an aim to resolve a problem.
+// Don't fear failure, give your solution to problems, keep trying until you succeed.
+// Listen carefully before reacting.
+// Intelligence is not restricted to a field, it's an attitude to understand everything you come across and apply the knowledge whenever needed.
+// “Why?” This is how intelligent people understand concepts.
+// Intelligent people make mistakes and learn key lessons, regretting is not for them.
+// Intelligence is about fast thinking, and you can't think fast until you practise thinking. (Overthinking may be a character of an intelligent person.)
+// If you are not born intelligent, it doesn't mean that you wouldn't die intelligent, and vice versa.
+
+// ************************************************How to identify when I am bored***********************************************************
+
+// Restlessness: Feeling fidgety, like you can't sit still or focus on a task.
+// Lack of interest: Finding that you're not engaged or interested in what you're currently doing.
+// Daydreaming: Frequently drifting off into unrelated thoughts or fantasies.
+// Checking the time: Repeatedly looking at the clock or your watch, hoping time will pass more quickly.
+// Procrastination: Avoiding tasks or responsibilities because you find them uninteresting or unchallenging.
+// Irritability: Becoming easily annoyed or frustrated with your current situation or surroundings.
+// Lack of motivation: Feeling unenthusiastic or unmotivated to start or complete tasks.
+// Increased phone or social media usage: Mindlessly scrolling through your phone or social media feeds to pass the time.
+// Seeking distractions: Constantly searching for something to do, even if it's unproductive or unrelated to your goals.
+// Physical cues: Yawning, sighing, or feeling physically tired despite not having expended much energy.
+// If you notice these signs, it's a good indicator that you might be bored. Boredom can be an opportunity to explore new interests, set goals, or engage in activities that stimulate your mind and bring you a sense of fulfillment. It can also be a signal that you need a change in your routine or environment to prevent monotony.
+
+
+
+
